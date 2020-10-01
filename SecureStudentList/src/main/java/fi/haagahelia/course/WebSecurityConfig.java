a28736e7d74426652aec44b93a1131a36217e073
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2020-10-01 17:58:48 +0300

    Updated deprecated classes to not deprecated

--- SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -6,17 +6,21 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,28 +36,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .logout()
           .permitAll();
     }
-    
+
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
         List<UserDetails> users = new ArrayList();
-    	UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
+
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        UserDetails user = User
+        		.withUsername("user")
+        		.password(passwordEncoder.encode("user"))
+        		.roles("USER")
+        		.build();
+
+        users.add(user);
+
+        user = User
+        		.withUsername("admin")
+        		.password(passwordEncoder.encode("admin"))
+        		.roles("USER", "ADMIN")
+        		.build();
 
     	users.add(user);
-    	
-    	user = User.withDefaultPasswordEncoder()
-                   .username("admin")
-                   .password("admin")
-                   .roles("USER", "ADMIN")
-                   .build();
-    	
-    	users.add(user);
-    	
+
         return new InMemoryUserDetailsManager(users);
     }
 
