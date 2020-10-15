95e202dbad4f28419adc3b21d5007b6660cbbdf7
Juslin Jukka <jusju@haaga-helia.fi>
2020-10-15 15:50:31 +0300

    updated to junit5

--- HelloTest/src/test/java/fi/haagahelia/course/HellotestApplicationTests.java
+++ HelloTest/src/test/java/fi/haagahelia/course/HellotestApplicationTests.java
@@ -2,11 +2,12 @@ package fi.haagahelia.course;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 import fi.haagahelia.course.web.HelloController;
 
@@ -16,7 +17,7 @@ import fi.haagahelia.course.web.HelloController;
  * @author h01270
  *
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class HellotestApplicationTests {
 
--- HelloTest/src/test/java/fi/haagahelia/course/WebLayerTest.java
+++ HelloTest/src/test/java/fi/haagahelia/course/WebLayerTest.java
@@ -1,18 +1,22 @@
 package fi.haagahelia.course;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -23,7 +27,7 @@ import org.springframework.test.web.servlet.MockMvc;
  * @author h01270
  *
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WebLayerTest {
