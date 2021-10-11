260c29e429b0439f81bb22397ffc80b3cf84b1e7
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2021-10-11 16:21:50 +0300

    Added true parameter to Spring sec conf

--- StudentJpaTest/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ StudentJpaTest/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .and()
       .formLogin()
           .loginPage("/login")
-          .defaultSuccessUrl("/studentlist")
+          .defaultSuccessUrl("/studentlist", true)
           .permitAll()
           .and()
       .logout()
