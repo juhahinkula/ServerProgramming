6d1bc9424e20ccd38c1267f91fb5f112f835cf89
Juslin Jukka <jusju@haaga-helia.fi>
2023-01-03 17:26:34 +0200

    spring boot updated to 3.0.1

---
+++ HelloRest/src/test/java/com/example/demo/RestdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
