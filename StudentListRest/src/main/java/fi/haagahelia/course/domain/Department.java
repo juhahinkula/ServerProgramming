2167f8322f834ae0a87f6a26fa9571ec83321f04
Juslin Jukka <jusju@haaga-helia.fi>
2023-01-03 17:43:03 +0200

    changed to spring boot 3.0.1 and java 17

--- StudentListRest/src/main/java/fi/haagahelia/course/domain/Department.java
+++ StudentListRest/src/main/java/fi/haagahelia/course/domain/Department.java
@@ -2,13 +2,14 @@ package fi.haagahelia.course.domain;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
