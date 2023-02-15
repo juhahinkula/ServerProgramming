652081253dc42af2bfed2cb5eedba9161e0efed4
jusju <jukka.juslin@haaga-helia.fi>
2023-02-15 09:39:38 +0200

    updated to spring boot 3

--- SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,15 +16,15 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+public class WebSecurityConfig  {
+
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+        .authorizeRequests().requestMatchers("/css/**").permitAll() // Enable css when logged out
         .and()
         .authorizeRequests().anyRequest().authenticated()
         .and()
@@ -39,7 +38,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Bean
-    @Override
     public UserDetailsService userDetailsService() {
         List<UserDetails> users = new ArrayList();
 
