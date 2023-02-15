f90bbddd8cfcce1fa154ec507c0dbf5506be8326
jusju <jukka.juslin@haaga-helia.fi>
2023-02-15 09:49:08 +0200

    updated to spring 3

--- SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -5,24 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import fi.haagahelia.course.web.UserDetailServiceImpl;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig  {
     @Autowired
     private UserDetailServiceImpl userDetailsService;	
 	
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+        .authorizeRequests().requestMatchers("/css/**").permitAll() // Enable css when logged out
         .and()
         .authorizeRequests()
           .anyRequest().authenticated()
