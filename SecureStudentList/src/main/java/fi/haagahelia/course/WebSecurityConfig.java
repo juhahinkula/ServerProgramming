360c33c4db90ef66c2df885a9b117ab2abcb9f2a
Juslin Jukka <jusju@haaga-helia.fi>
2023-03-06 12:03:38 +0200

    fixed sprinb boot 3

--- SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentList/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package fi.haagahelia.course;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,51 +13,37 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig  {
+public class WebSecurityConfig {
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .authorizeRequests().requestMatchers("/css/**").permitAll() // Enable css when logged out
-        .and()
-        .authorizeRequests().anyRequest().authenticated()
-        .and()
-      .formLogin()
-          .defaultSuccessUrl("/studentlist", true)
-          .permitAll()
-          .and()
-      .logout()
-          .permitAll();
-    }
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().requestMatchers("/css/**").permitAll() // Enable css when logged out
+				.anyRequest().authenticated().and().formLogin().defaultSuccessUrl("/studentlist", true).permitAll()
+				.and().logout().permitAll().and().httpBasic();
+		return http.build();
+	}
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		System.out.println("in-memory users - luodaan näitä kaksi kappaletta");
+		List<UserDetails> users = new ArrayList<UserDetails>();
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList();
+		PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserDetails user1 = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
 
-        UserDetails user = User
-        		.withUsername("user")
-        		.password(passwordEncoder.encode("user"))
-        		.roles("USER")
-        		.build();
+		users.add(user1);
 
-        users.add(user);
+		UserDetails user2 = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN")
+				.build();
 
-        user = User
-        		.withUsername("admin")
-        		.password(passwordEncoder.encode("admin"))
-        		.roles("USER", "ADMIN")
-        		.build();
+		users.add(user2);
 
-    	users.add(user);
-
-        return new InMemoryUserDetailsManager(users);
-    }
-
+		return new InMemoryUserDetailsManager(users);
+	}
 }
