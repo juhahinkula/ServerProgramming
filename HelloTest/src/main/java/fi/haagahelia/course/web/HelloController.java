f2810e11e4efb6dae846bb12721d3a77eb5ffb93
Juslin Jukka <jusju@haaga-helia.fi>
2023-03-07 09:57:44 +0200

    Removed old User class and changed to Spring Boot 3

--- HelloTest/src/main/java/fi/haagahelia/course/web/HelloController.java
+++ HelloTest/src/main/java/fi/haagahelia/course/web/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+	@Controller
 public class HelloController {
 
     @RequestMapping("/")
--- StudentJpaTest/src/main/java/fi/haagahelia/course/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.haagahelia.course.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
-}
