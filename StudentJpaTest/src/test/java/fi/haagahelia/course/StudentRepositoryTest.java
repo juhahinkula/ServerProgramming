1ae4f52fde5fb17569bf4a0ba019c2cc614155de
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2023-03-14 09:46:26 +0200

    Update StudentRepositoryTest.java

--- StudentJpaTest/src/test/java/fi/haagahelia/course/StudentRepositoryTest.java
+++ StudentJpaTest/src/test/java/fi/haagahelia/course/StudentRepositoryTest.java
@@ -19,7 +19,7 @@ import fi.haagahelia.course.domain.StudentRepository;
 //@DataJpaTest
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = StudentApplication.class)
+@SpringBootTest(classes = StudentListApplication.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StudentRepositoryTest {
 
